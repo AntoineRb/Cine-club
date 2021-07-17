d1b85cc219872c616d3111d075c1840dc7817e80
A.R <87040192+AntoineRb@users.noreply.github.com>
2021-07-17 18:05:19 +0200

    Update movie.py

--- movie.py
+++ movie.py
@@ -9,7 +9,6 @@ CUR_DIR = os.path.dirname(__file__) #Path to current directory
 DATA_FILE = os.path.join(CUR_DIR, "movies.json") #Path to Json file
 
 def get_movies():
-    """récupère une instance de la classe Movie pour chaque tire de films"""
     with open(DATA_FILE, "r", encoding='utf-8') as f:
         movies_titles = json.load(f)
 
